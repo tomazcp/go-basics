559b6813e33fdfc6f257578bb72c4a9a47d49837
Tomás Pinto <tomaspinto@protonmail.ch>
2019-06-22 18:22:32 +0100

    more modules and exercises

---
+++ struct/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+// type person with underlying type struct
+type person struct {
+	first string
+	last  string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func main() {
+	sa1 := secretAgent{
+		person: person{
+			first: "James",
+			last:  "Bond",
+		},
+		ltk: true,
+	}
+
+	p2 := person{
+		first: "Miss",
+		last:  "Moneypenny",
+	}
+
+	fmt.Println(sa1)
+	fmt.Println(p2)
+
+	// anonymous struct
+	anonStruct := struct {
+		first string
+		last  string
+		age   int
+	}{
+		first: "James",
+		last:  "Bond",
+		age:   43,
+	}
+	fmt.Println(anonStruct)
+}
